3f8fae88728c46a9c037773f352988186331a75e
fabianNav <fn39001@gmail.com>
2024-04-19 19:02:05 -0300

    Terminé con la logica de cambiar de tema, ahora le seguire dando estilo con CSS

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 import './css/style.css'
+import { cambiarTema } from './src/theme'
+
+cambiarTema();
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,18 @@
 const switchButton = document.getElementById('switch');
 
+export function cambiarTema() {
 
+    const colorSystem = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        const setTheme = (theme) => {
+            document.documentElement.setAttribute('data-theme', theme);
+            localStorage.setItem('theme', theme);
+        }
+
+    switchButton.addEventListener('click', () => {
+        switchButton.classList.toggle('active');
+        let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+        setTheme(switchToTheme);
+    })
+    setTheme(localStorage.getItem('theme') || colorSystem);
+}
 
