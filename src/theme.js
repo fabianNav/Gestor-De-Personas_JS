46ed23490f8f15cc3be9eb77dab995bff34ff4e9
fabianNav <fn39001@gmail.com>
2024-04-19 19:21:42 -0300

    Le hice algunos ajustes al modo oscuro para que quede mas piola

--- src/theme.js
+++ src/theme.js
@@ -3,15 +3,28 @@ const switchButton = document.getElementById('switch');
 export function cambiarTema() {
 
     const colorSystem = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        const setTheme = (theme) => {
-            document.documentElement.setAttribute('data-theme', theme);
-            localStorage.setItem('theme', theme);
+    const setTheme = (theme) => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }
+    function insertLogo() {
+        let readLs = localStorage.getItem('theme');
+        if (readLs === 'light') {
+            switchButton.innerHTML = `
+            <span><i class="fa-regular fa-sun"></i></span>
+            `
+        } else if (readLs === 'dark') {
+            switchButton.innerHTML = `
+            <span><i class="fa-regular fa-moon"></i></span>
+            `
         }
+    }
 
     switchButton.addEventListener('click', () => {
         switchButton.classList.toggle('active');
         let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
         setTheme(switchToTheme);
+        insertLogo();
     })
     setTheme(localStorage.getItem('theme') || colorSystem);
 }
