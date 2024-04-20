6272a92f115162edc85396019fd89e8c11e1a33e
fabianNav <fn39001@gmail.com>
2024-04-19 21:39:17 -0300

    Punto de guardado

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import './css/style.css'
+import { insertarDatosDom } from './src/datosFormDom';
 import { cambiarTema } from './src/theme'
 
 cambiarTema();
 
+const submit = document.getElementById('formPersona');
+submit.addEventListener('submit', (e) => {
+  e.preventDefault();
+  insertarDatosDom();
+});
--- src/datosFormDom.js
+++ src/datosFormDom.js
@@ -1,9 +1,50 @@
-const nombre = document.getElementById('nombre').value;
-const apellido = document.getElementById('apellido').value;
-const edad = document.getElementById('edad').value;
-const nacimiento = document.getElementById('nacimiento').value;
-const correo = document.getElementById('correo').value;
-const provincia = document.getElementById('provincia').value;
-const localidad = document.getElementById('localidad').value;
-const calle = document.getElementById('calle').value;
-const direccionNumero = document.getElementById('direccion').value;
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const edad = document.getElementById('edad');
+const nacimiento = document.getElementById('nacimiento');
+const correo = document.getElementById('correo');
+const provincia = document.getElementById('provincia');
+const localidad = document.getElementById('localidad');
+const calle = document.getElementById('calle');
+const direccionNumero = document.getElementById('direccion');
+const containerDatos = document.getElementById('containerDatos');
+
+
+export function insertarDatosDom() {
+    let datosArr = [];
+
+    const nuevaPersona = {
+        id: (Math.random() * 200).toString(32).slice(3),
+        nombre: nombre.value,
+        apellido: apellido.value,
+        edad: edad.value,
+        nacimiento: nacimiento.value,
+        correo: correo.value,
+        provincia: provincia.value,
+        localidad: localidad.value,
+        calle: calle.value,
+        direccionNumero: direccionNumero.value
+    }
+
+    datosArr = datosArr.push(nuevaPersona);
+
+    const datosPersonas = datosArr.map(elemento => {
+        return `
+                                    <tr id="${elemento.id}">
+                                        <td>${elemento.nombre}</td>
+                                        <td>${elemento.apellido}</td>
+                                        <td>${elemento.edad}</td>
+                                        <td>${elemento.nacimiento}</td>
+                                        <td>${elemento.correo}</td>
+                                        <td>${elemento.provincia}</td>
+                                        <td>${elemento.localidad}</td>
+                                        <td>${elemento.calle}</td>
+                                        <td>${elemento.direccionNumero}</td>
+                                    </tr>
+    
+                            
+                    `
+    })
+    containerDatos.innerHTML += datosPersonas.join("");
+
+}
