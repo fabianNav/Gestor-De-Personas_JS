c4273e583614810df898a55929ce1dd4e8fc3d20
fabianNav <fn39001@gmail.com>
2024-04-22 19:11:19 -0300

    Personalicé un poco la tabla

--- src/datosFormDom.js
+++ src/datosFormDom.js
@@ -1,3 +1,4 @@
+
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 const edad = document.getElementById('edad');
@@ -9,9 +10,9 @@ const calle = document.getElementById('calle');
 const direccionNumero = document.getElementById('direccion');
 const containerDatos = document.getElementById('containerDatos');
 
-
 export function insertarDatosDom() {
     let datosArr = [];
+    let personasArr = [];
 
     const nuevaPersona = {
         id: (Math.random() * 200).toString(32).slice(3),
@@ -38,56 +39,47 @@ export function insertarDatosDom() {
             e.provincia &&
             e.localidad &&
             e.calle &&
-            e.direccionNumero !== "") && nacimiento) {
-            Swal.fire({
+            e.direccionNumero !== "")) {
+                Swal.fire({
                 position: "center",
                 icon: "success",
                 title: "Persona agregada",
                 showConfirmButton: false,
                 background: 'var(--background)',
-                color: 'var(--text)'
+                color: 'var(--text)',
+                showConfirmButton:true,
+                timer: 1500
             });
             return `
-                        <thead>
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Apellido</th>
-                                <th>Edad</th>
-                                <th>Nacimiento</th>
-                                <th>Correo</th>
-                                <th>Provincia</th>
-                                <th>Localidad</th>
-                                <th>Calle</th>
-                                <th>Numero De Casa</th>
+                        
+                            <tr id="${e.id}" class="trTbody">
+                                <td class="tdTable">${e.nombre}</td>
+                                <td class="tdTable">${e.apellido}</td>
+                                <td class="tdTable">${e.edad}</td>
+                                <td class="tdTable">${e.nacimiento}</td>
+                                <td class="tdTable">${e.correo}</td>
+                                <td class="tdTable">${e.provincia}</td>
+                                <td class="tdTable">${e.localidad}</td>
+                                <td class="tdTable">${e.calle}</td>
+                                <td class="tdTable">${e.direccionNumero}</td>
+                                <td><button class="eliminar">X</button></td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <tr id="${e.id}">
-                                <td>${e.nombre}</td>
-                                <td>${e.apellido}</td>
-                                <td>${e.edad}</td>
-                                <td>${e.nacimiento}</td>
-                                <td>${e.correo}</td>
-                                <td>${e.provincia}</td>
-                                <td>${e.localidad}</td>
-                                <td>${e.calle}</td>
-                                <td>${e.direccionNumero}</td>
-                            </tr>
-                        </tbody>
                     `;
-        } else {
+        }
+        else {
             Swal.fire({
                 icon: "error",
                 title: "Oops...",
                 text: "Parece que hay datos sin llenar",
                 footer: 'Todos los datos son obligatorios',
                 background: 'var(--background)',
-                color: 'var(--text)'
+                color: 'var(--text)',
+                timer: 2000
             });
-
         }
 
     });
     containerDatos.innerHTML += datosPersonas.join("");
 
 }
+
